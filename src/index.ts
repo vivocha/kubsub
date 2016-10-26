1c9f410638d048bfa04c862ee7e54ad332cb2c5d
Federico Pinna <fpinna@vivocha.com>
2016-10-26 14:31:02 +0200

    feat(module): export Client and ClientOptions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ClientOptions, Client } from './client';
+export { ClientOptions, Client } from './client';
 
 export function create(opts:ClientOptions):Promise<Client> {
   return (new Client(opts)).connect();
