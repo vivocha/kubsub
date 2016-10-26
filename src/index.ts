d9d6ab1cc25f7bc1c6181121107642540e837fcf
Federico Pinna <fpinna@vivocha.com>
2016-10-26 16:14:49 +0200

    feat(module): export Channel

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ClientOptions, Client } from './client';
 export { ClientOptions, Client } from './client';
+export { Channel } from './channel';
 
 export function create(opts:ClientOptions):Promise<Client> {
   return (new Client(opts)).connect();
